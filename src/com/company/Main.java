82babce5507a38ddd93cebdc9be611357a444886
Nikita Fomin <97741120+r1xelraze@users.noreply.github.com>
2022-02-20 19:26:31 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int n = scan.nextInt();
+        int [] nums= new int [n];
+        System.out.println("Введите элементы массива: ");
+        for (int i =0; i<n; i++){
+            nums[i]=scan.nextInt();
+        }
+        int min=nums[0];
+        for(var i=0; i<n; i++)
+        {
+            if (min>nums[i])
+            {
+                min=nums[i];
+            }
+        }
+        int S=0;
+        for(var i=0; i<n; i++)
+        {
+            S=S+nums[i];
+        }
+        System.out.println("Сумма минимального и среднего значения:  " +(S/n+min));
+
+
+    }
+}
